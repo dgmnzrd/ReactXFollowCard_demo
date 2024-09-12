ec6326a0917e28076098c05e2c59b23697af5559
Marcos Juarez <>
2024-09-11 22:48:58 -0600

    Update: Portv3

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export function App () {
   const [array, setArray] = useState([])
 
   const fetchAPI = async () => {
-    const response = await axios.get("127.0.0.1:18012/api/users")
+    const response = await axios.get("https://reactxfollowcard-server-demo-1.onrender.com:10000/api/users")
     setArray(response.data.users)
   }
 
